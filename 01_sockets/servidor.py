bff870243d07f484da2d28380e397255d0551f12
Tu Nombre <tu@email.com>
2025-08-01 20:51:51 -0500

    Actualizacion

--- 01_sockets/servidor.py
+++ 01_sockets/servidor.py
@@ -1,1 +1,14 @@
-# Servidor
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Servidor escuchando en {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print('Conectado por', addr)
+        data = conn.recv(1024)
+        print('Mensaje recibido:', data.decode())
